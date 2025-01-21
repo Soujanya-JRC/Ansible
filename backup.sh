aee12916dbb913f38431ff2994ffd16d2e930b8b
unknown <chitri.sozanya83@gmail.com>
2025-01-21 22:18:22 +0530

    backup shell-script

---
+++ backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #if user is not providing numberof days we take 14 days by default
+
+LOGS_FOLDER="/var/log/shell-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+    exit 1
+}
+if [ $# -le 2]
+then 
+    USAGE
+fi
+
+
